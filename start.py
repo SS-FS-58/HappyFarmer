41e32f5a212ce436af770e22027749dc958a8558
Mtchuikov <71787586+Mtchuikov@users.noreply.github.com>
2021-02-01 13:31:52 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,14 +1,21 @@
 #!C:\Users\Misha\AppData\Local\Programs\Python\Python39\python.exe
 # coding: utf8
 
-import subprocess, sys
+import subprocess, sys, platform
 
-print("""╔╗╔╗─────────╔══╗
+if platform.system()=="Windows":
+    subprocess.Popen("cls", shell=True).communicate()
+else:
+    print("\033c", end="")
+
+print("""\n╔╗╔╗─────────╔══╗
 ║╚╝╠═╗╔═╦═╦╦╗║═╦╩╗╔╦╦══╦═╦╦╗
 ║╔╗║╬╚╣╬║╬║║║║╔╣╬╚╣╔╣║║║╩╣╔╝
 ╚╝╚╩══╣╔╣╔╬╗║╚╝╚══╩╝╚╩╩╩═╩╝
 ──────╚╝╚╝╚═╝""")
 
+
 while True:
     process = subprocess.Popen([sys.executable, "main.py"])
     process.wait()
+
